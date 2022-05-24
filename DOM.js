489bbcd2648369b7287306e3be7a145d855cafe1
Surya Pratap <garfieldthesurya01@gmail.com>
2022-05-24 10:16:31 +0530

    different methods to access html elements

--- DOM.js
+++ DOM.js
@@ -1,5 +1,11 @@
 // DOM manupilation
 
 // 1st:- getting Element by id
-const dom = document.getElementById("dom");
-console.log(dom);
+const domOne = document.getElementById("dom");
+console.log(domOne);
+
+// We can also access html tags by using document.getElementsByTagName but remember here we are writting elements not element so avoid using this method because it can create confusion.
+
+// The best way to manupilate html elements is by using querySelector method.
+const domTwo = document.querySelector("#dom");
+console.log(domTwo);
